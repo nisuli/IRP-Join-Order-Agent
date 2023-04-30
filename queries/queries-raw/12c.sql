e631a863c5c234895bd38f99e0514d6fa378a104
Mahsun ALTIN <43652243+mahsunaltin@users.noreply.github.com>
2023-04-30 22:02:11 +0300

    Add files via upload

---
+++ queries/queries-raw/12c.sql
@@ -0,0 +1,25 @@
+SELECT MIN(cn.name) AS movie_company,
+       MIN(t.title) AS mainstream_movie
+FROM company_name AS cn,
+     company_type AS ct,
+     info_type AS it1,
+     info_type AS it2,
+     movie_companies AS mc,
+     movie_info AS mi,
+     title AS t
+WHERE cn.country_code = '[us]'
+  AND ct.kind = 'production companies'
+  AND it1.info = 'genres'
+  AND it2.info = 'rating'
+  AND mi.info IN ('Drama',
+                  'Horror',
+                  'Western',
+                  'Family')
+  AND t.production_year BETWEEN 2000 AND 2010
+  AND t.id = mi.movie_id
+  AND mi.info_type_id = it1.id
+  AND t.id = mc.movie_id
+  AND ct.id = mc.company_type_id
+  AND cn.id = mc.company_id
+  AND mc.movie_id = mi.movie_id;
+
---
+++ queries/queries-raw/18a.sql
@@ -0,0 +1,20 @@
+SELECT MIN(mi.info) AS movie_budget,
+       MIN(t.title) AS movie_title
+FROM cast_info AS ci,
+     info_type AS it1,
+     info_type AS it2,
+     movie_info AS mi,
+     name AS n,
+     title AS t
+WHERE ci.note IN ('(producer)',
+                  '(executive producer)')
+  AND it1.info = 'budget'
+  AND it2.info = 'votes'
+  AND n.gender = 'm'
+  AND n.name LIKE '%Tim%'
+  AND t.id = mi.movie_id
+  AND t.id = ci.movie_id
+  AND ci.movie_id = mi.movie_id
+  AND n.id = ci.person_id
+  AND it1.id = mi.info_type_id;
+
